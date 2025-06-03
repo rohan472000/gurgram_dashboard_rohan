be65a74547e38828012caae303631f576d8d41b1
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2025-06-03 16:27:06 +0530

    Update run.py

--- run.py
+++ run.py
@@ -132,5 +132,5 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
 
